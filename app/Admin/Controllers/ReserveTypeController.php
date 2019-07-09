708ba710a8db9a5582a0e8210f628569c5cb2fb5
fengxin <502251245@qq.com>
2019-07-09 17:11:22 +0800

    2019 7 9 修改后台网订类型bug

--- app/Admin/Controllers/ReserveTypeController.php
+++ app/Admin/Controllers/ReserveTypeController.php
@@ -41,7 +41,7 @@ class ReserveTypeController extends AdminController{
         $grid=new Grid(new ReserveType());
         $grid->column('id','ID')->sortable();
         $grid->column('reserve_type_name','类型名称')->editable();
-        $grid->column('reserver_type_image','类型封面')->lightbox(['width' => 50, 'height' => 50]);
+        $grid->column('reserve_type_image','类型封面')->lightbox(['width' => 50, 'height' => 50]);
         $grid->actions(function ($actions) {
             $actions->disableView();
             $actions->disableDelete();
