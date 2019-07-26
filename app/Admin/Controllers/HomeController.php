dc4f5cb826a6adbd0e988605b0123f0dfe1f353a
fengxin <502251245@qq.com>
2019-07-26 17:55:31 +0800

    2019 7 26 新增首页仪表盘

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
 use Encore\Admin\Controllers\AdminController;
-use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
@@ -14,22 +13,21 @@ class HomeController extends AdminController
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
-            ->description('Description...')
+            ->title('首页')
+            ->description('仪表盘...')
            //->row(Dashboard::title())
             ->row(function (Row $row) {
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                $row->column(3, function (Column $column) {
+                    $column->append(Dashboard::todayOrder());
+                });
+                $row->column(3, function (Column $column) {
+                    $column->append(Dashboard::todayOrder(2,'green'));
                 });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
+                $row->column(3, function (Column $column) {
+                   // $column->append(Dashboard::dependencies());
                 });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
+            })
+            ->row(Dashboard::environment());
     }
 }
